82849555a55abef2023706a90be1429a674a9cdc
David Oliver Sipos <davidsipos1002@gmail.com>
2023-08-14 20:15:03 +0300

    Added CMake build

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,3 +1,7 @@
-./build.sh
+./clean.sh
+echo 'Building using CMake...'
+cmake -S . -B cmake_build
+cmake --build cmake_build
+cmake --install cmake_build
+echo 'Generating image...'
 ./gen.sh
-./run.sh
--- src/print/print.c
+++ src/print/print.c
@@ -1,5 +1,5 @@
 #include <bootloader/print.h>
 
 EFI_STATUS print(EFI_SYSTEM_TABLE *ST) {
-    return ST->ConOut->OutputString(ST->ConOut, L"Hello World\r\n");
+    return ST->ConOut->OutputString(ST->ConOut, L"Hello World, I am David's custom bootloader\r\n");
 }
