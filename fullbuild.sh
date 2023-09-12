0f5cd3f2ad95989d6a965c75d8f5918aa69674ba
David Oliver Sipos <davidsipos1002@gmail.com>
2023-09-12 18:32:38 +0300

    Move cmake_build folder to build/cmake_build

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,7 +1,7 @@
 ./clean.sh
 echo 'Building using CMake...'
-cmake -Dbasic_logging=ON -Dextensive_logging=OFF -Dprint_config=ON -DCMAKE_BUILD_TYPE=Debug -S . -B cmake_build
-cmake --build cmake_build
-cmake --install cmake_build
+cmake -Dbasic_logging=ON -Dextensive_logging=OFF -Dprint_config=ON -DCMAKE_BUILD_TYPE=Debug -S . -B build/cmake_build
+cmake --build build/cmake_build
+cmake --install build/cmake_build
 echo 'Generating image...'
 ./gen.sh
